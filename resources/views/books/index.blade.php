6254f163d2ced5e487e548224b173fe57eeca908
tales.falcao <taleslazarini@hotmail.com>
2025-03-01 18:10:49 -0300

    Book filter by title

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,11 @@
 @section('content')
   <h1 class="mb-10 text-2xl">Books</h1>
 
-  <form></form>
+  <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+    <input class="input h-10" type="text" name="title" placeholder="Search by title..." value="{{ request('title') }}">
+    <button class="btn h-10" type="submit">Search</button>
+    <a class="btn h-10" href="{{ route('books.index') }}">Clear</a>
+  </form>
 
   <ul>
     @forelse ($books as $book)
