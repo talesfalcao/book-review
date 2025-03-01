3277431dd841d4caa321cf74a597e814b1b3540a
tales.falcao <taleslazarini@hotmail.com>
2025-03-01 14:46:05 -0300

    scope query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -11,5 +12,9 @@ class Book extends Model
 
     public function reviews() {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitleLike(Builder $query, string $title): Builder {
+        return $query->where('title', 'LIKE', '%'.$title.'%');
     }
 }
